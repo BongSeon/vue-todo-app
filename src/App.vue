ca67450fc692b8ddd8d32ba49edd02818b36200a
Aestas <hawhawba@gmail.com>
2021-04-19 01:34:02 +0900

    ESLint, Prettier 설정 적용

--- src/App.vue
+++ src/App.vue
@@ -1,39 +1,43 @@
 <template>
-    <div id="app">
-      <todo-header></todo-header>
-      <todo-input></todo-input>
-      <todo-list></todo-list>
-      <todo-footer></todo-footer>
-    </div>
+  <div id="app">
+    <todo-header></todo-header>
+    <todo-input></todo-input>
+    <todo-list></todo-list>
+    <todo-footer></todo-footer>
+  </div>
 </template>
 
 <script>
-import TodoHeader from './components/TodoHeader.vue'
-import TodoInput from './components/TodoInput.vue'
-import TodoList from './components/TodoList.vue'
-import TodoFooter from './components/TodoFooter.vue'
-
+import TodoHeader from "./components/TodoHeader.vue";
+import TodoInput from "./components/TodoInput.vue";
+import TodoList from "./components/TodoList.vue";
+import TodoFooter from "./components/TodoFooter.vue";
 
 export default {
   components: {
-    'TodoHeader': TodoHeader,
-    'TodoInput': TodoInput,
-    'TodoList': TodoList,
-    'TodoFooter': TodoFooter
+    TodoHeader: TodoHeader,
+    TodoInput: TodoInput,
+    TodoList: TodoList,
+    TodoFooter: TodoFooter
   }
-}
+};
+// Prettier 사용 방법 : 쉬프트,옵션,F 
 </script>
 
 <style>
-  body{
-          text-align: center;
-          background-color: #F6F6F8;
-  }
-    input {
-      border-style: groove;
-      width: 200px;
+body {
+  text-align: center;
 
-    }
-    button{border-style: groove;}
-      .shadow{ box-shadow: 5px 10px 10px rgba(0,0,0,0.03);}
+  background-color: #f6f6f8;
+}
+input {
+  border-style: groove;
+  width: 200px;
+}
+button {
+  border-style: groove;
+}
+.shadow {
+  box-shadow: 5px 10px 10px rgba(0, 0, 0, 0.03);
+}
 </style>
